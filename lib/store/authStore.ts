3699278b77a68ff33458e368bfad4ee389e5bfe4
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-12-25 18:39:13 +0100

    modified:   lib/store/authStore.ts

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -5,16 +5,17 @@ type AuthStore = {
   user: User | null;
   isAuthenticated: boolean;
   setUser: (user: User) => void;
-  logout: () => void;
   reset: () => void;
+  logout: () => void;
 };
 
 const useAuthStore = create<AuthStore>()((set) => ({
   user: null,
   isAuthenticated: false,
   setUser: (user) => set({ user, isAuthenticated: true }),
-  logout: () => set({ user: null, isAuthenticated: false }),
   reset: () => set({ user: null, isAuthenticated: false }),
+  logout: () => set({ user: null, isAuthenticated: false }),
+  
 }));
 
 export default useAuthStore;
